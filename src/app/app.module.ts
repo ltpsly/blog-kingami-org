17cecf5588dc1a9c62fd2833f0ad6ea625fae66e
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2018-04-01 23:47:28 +0530

    routes module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/home/home.component';
 import { NavbarComponent } from './main/shared/components/navbar/navbar.component';
-
+import { AppRoutesModule } from './app-routes.module';
+import { UserComponent } from './main/user/user.component';
+import { BlogComponent } from './main/blog/blog.component';
+import { AboutComponent } from './main/shared/components/about/about.component';
+import { NextComponent } from './main/shared/components/next/next.component';
+import { FooterComponent } from './main/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    HomeComponent,
+    NavbarComponent,
+    UserComponent,
+    BlogComponent,
+    AboutComponent,
+    NextComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
