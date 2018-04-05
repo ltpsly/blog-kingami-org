93302a1c1e27a587f2ff372f6f7aab5c7a960c90
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2018-04-05 23:24:41 +0530

    add models for app

---
+++ src/app/main/shared/models/article.interface.ts
@@ -0,0 +1,14 @@
+import { Profile } from './profile.interface';
+
+export interface Article {
+    slug: string;
+    title: string;
+    description: string;
+    body: string;
+    tagList?: (string)[] | null;
+    createdAt: string;
+    updatedAt: string;
+    favorited: boolean;
+    favoritesCount: number;
+    author: Profile;
+}
