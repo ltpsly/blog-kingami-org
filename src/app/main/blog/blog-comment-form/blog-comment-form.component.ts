42e463c57505d2fc8fd837624327c928d9e00e87
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2018-04-08 17:36:38 +0530

    blog impl

---
+++ src/app/main/blog/blog-comment-form/blog-comment-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-comment-form',
+  templateUrl: './blog-comment-form.component.html',
+  styleUrls: ['./blog-comment-form.component.css']
+})
+export class BlogCommentFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
